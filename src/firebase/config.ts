a87c103e39667740f556dee14f9662e7092a8ddc
Pramesh Luitel <luitelpramesh@gmail.com>
2026-08-17 13:58:47 +0545

    security: move Firebase API key to env var

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6133801027-aa2fc",
   "appId": "1:912055863494:web:e655007380b0777ab16fc7",
-  "apiKey": "AIzaSyDXcMUi7FwriqARLvOkg8nPhx_mIpDYBPE",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   "authDomain": "studio-6133801027-aa2fc.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "912055863494"
